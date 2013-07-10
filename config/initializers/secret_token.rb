1653641acdc9ccfefee5ffae9dccae9ed192b742
Felipe Vieira <nvieirafelipe@gmail.com>
2013-07-10 17:01:02 -0300

    Start authorizati

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Authorizati::Application.config.secret_key_base = '61edcf2b612e0b52a223837eda7aafecd725b96882bb2f7c745f7eb05c4f1dc22b418c9e3c76389bc572b411849aefd779d9d48580fb41bc5c85579f47aba6b1'
